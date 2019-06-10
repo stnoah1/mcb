e005309a5f0216a96005883e91df3edcb4cccaca
hg <stnoah1@gmail.com>
2019-06-10 19:33:08 -0400

    update requirements and gitignore

--- scrap.py
+++ scrap.py
@@ -2,7 +2,9 @@ import logging
 from datetime import datetime
 import db
 from scrapper import grab_cad, dw
+from utils import make_dir
 
+make_dir('log')
 logging.basicConfig(filename=f'log/{datetime.now().strftime("%y%m%d_%H%M%S")}.log', level=logging.ERROR)
 
 
@@ -11,18 +13,11 @@ def search(keyword, websites=['grabcad', '3dw']):
 
     if 'grabcad' in websites:
         logging.info('graCAD')
-        try:
-            grab_cad.run(keyword=keyword, softwares=['obj', 'stl', 'ptc-creo-parametric', 'solidworks'])
-        except Exception as e:
-            logging.error(f'[{keyword}]:{e}')
+        grab_cad.run(keyword=keyword, softwares=['obj', 'stl', 'ptc-creo-parametric', 'solidworks'])
 
     if '3dw' in websites:
-
         logging.debug('3DW')
-        try:
-            dw.run(keyword=keyword)
-        except Exception as e:
-            logging.error(f'[{keyword}]:{e}')
+        dw.run(keyword=keyword)
 
 
 def search_keywords_db():
@@ -32,4 +27,7 @@ def search_keywords_db():
         search(keyword['name'], websites=['3dw'])
 
 
-search_keywords_db()
+if __name__ == "__main__":
+    for keyword in ['lock', 'multimeter', 'opener', 'pencil', 'protractor', 'scissors',
+                    'sharpie', 'spoon', 'stapler', 'tong', 'tweezer', 'whisk']:
+        search(keyword, websites=['3dw'])
--- utils.py
+++ utils.py
@@ -1,6 +1,18 @@
 import os
+import shutil
+
+from config import grabcad_path
 
 
 def make_dir(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
+
+
+def clean_dir(directory):
+    for subdir in os.listdir(directory):
+        keyword_path = os.path.join(directory, subdir)
+        for item in os.listdir(keyword_path):
+            item_path = os.path.join(keyword_path, item)
+            if os.path.isdir(item_path):
+                shutil.rmtree(item_path)
