d436f660af822c05dee626a794d3d838b9b29b3d
hg <stnoah1@gmail.com>
2019-06-10 19:32:37 -0400

    create img function add

--- create_img.py
+++ create_img.py
@@ -1,56 +1,15 @@
-import glob
 import os
-from os.path import join
 
-import numpy as np
-from scipy.misc import imread, imsave, imresize
-from tqdm import tqdm
+from config import dw_path, grabcad_path
+from mat_api import MatlabAPI
 
 
-# todo: bugfix => some images are not printed.
+def make_image(path):
+    MatlabAPI().make_image(path)
 
-imgroot = 'obj_img'
-_saveroot = 'imgdump'
 
-if not os.path.isdir(_saveroot):
-    os.makedirs(_saveroot)
-
-for nameclass in tqdm(os.listdir('labeled')):
-    # For square
-    objs = glob.glob(f'labeled/{nameclass}/*.obj')
-    nums = len(objs)
-    if not nums == 0:
-        rows = int(np.ceil(np.log2(nums)))
-        if rows == 0:
-            rows = 1
-
-        imrows = []
-        Refresh = True
-        for idx, obj in enumerate(objs):
-            imgpath = join(imgroot, os.path.basename(obj).replace('.obj', '.png'))
-            if idx == 0 or Refresh:
-                img = imread(imgpath)
-                img = imresize(img, (128, 128))
-                Refresh = False
-            else:
-                tmp = imread(imgpath)
-                tmp = imresize(tmp, (128, 128))
-                img = np.concatenate([img, tmp], axis=1)
-
-            if idx % rows == 0 and not idx == 0:
-                imrows.append(img)
-                Refresh = True
-
-        for idx, tmp in enumerate(imrows):
-            if idx == 0:
-                img = tmp
-                ref = img.shape
-            else:
-                h, w, c = tmp.shape
-                if ref[1] != w:
-                    tmp = np.concatenate([tmp, np.ones([h, ref[1] - w, c]) * 255], axis=1)
-
-                img = np.concatenate([img, tmp], axis=0)
-
-        savepath = join(_saveroot, nameclass + '.png')
-        imsave(savepath, img)
+for cad_dir in [grabcad_path, dw_path]:
+    for directory in os.listdir(cad_dir):
+        cat_dir = os.path.join(cad_dir, directory)
+        if os.path.isdir(cat_dir):
+            make_image(cat_dir)
